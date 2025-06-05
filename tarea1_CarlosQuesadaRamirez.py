6a3d440b75f67198443bd39c67ccb4191f8847ac
Carlos Eduardo Quesada Ramirez <carloseduardoquesadaramirez@gmail.com>
2025-06-04 18:03:30 -0600

    Implementacion funciones des de ingreso de datos

--- tarea1_CarlosQuesadaRamirez.py
+++ tarea1_CarlosQuesadaRamirez.py
@@ -18,4 +18,36 @@ def metodoIngresoVector():
             print("1. Ingresar por magnitud y dirección")
             print("2. Imgresar por componentes")
             print("Respuesta: ")
+            opcion = input()
+
+            if opcion == '1':
+                return ingresoPorMagnitudDireccion()
+            elif opcion == '2':
+                return ingresoPorComponentes()
+            else:
+                print("Opción inválida. Por favor, intente de nuevo.")
+
+def ingresoPorMagnitudDireccion():
+    try:
+        magnitud = float(input("Ingrese la magnitud del vector: "))
+        angulo = float(input("Ingrese el ángulo del vector (en grados): "))
+        anguloRadiantes = math.radians(angulo)
+        ladoX = magnitud * math.cos(anguloRadiantes)
+        ladoY = magnitud * math.sin(anguloRadiantes)
+        return (ladoX, ladoY)
+    except ValueError:
+        print("El número que ingresó, no es correcto. Asegúrese de ingresar números válidos.")
+        return ingresoPorMagnitudDireccion()
+    
+
+def ingresoPorComponentes():
+    try:
+        ladoX = float(input("Ingrese el componente X del vector: "))
+        ladoY = float(input("Ingrese el componente Y del vector: "))
+        return (ladoX, ladoY)
+    except ValueError:
+        print("El número que ingresó, no es correcto. Asegúrese de ingresar números válidos.")
+        return ingresoPorComponentes()
+
+        
         
